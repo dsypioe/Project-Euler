403c8dff2dd35e52417ab73e6c63875255681277
dsypioe <33521953+dsypioe@users.noreply.github.com>
2020-02-17 18:37:38 -0500

    Updated Problem 1 and started Problem 2

--- python/problem1.py
+++ python/problem1.py
@@ -9,7 +9,10 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def mult3or5(n):
+def solution(n):
 	return sum([x for x in range(n) if x % 3 == 0 or x % 5 == 0])
+	
 
+def mult30r5(n):
+	return [x for x in range(n) if x % 3 == 0 or x % 5 == 0]
 
