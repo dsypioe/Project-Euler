47d29b803b5551ec2dbd8f65795a6b6bf0f5fd24
dsypioe <33521953+dsypioe@users.noreply.github.com>
2020-02-19 16:15:40 -0500

    solved problem 3 - python

--- python/problem3.py
+++ python/problem3.py
@@ -4,9 +4,32 @@
 
 # What is the largest prime factor of the number 600851475143 ?
 
-
+# returns the factors of a number in a list
 def factors(n):
-	factors = [x for x in range(n) if n % x == 0]
+	return [x for x in range(1, n+1) if n % x == 0]
 
+# checks if a number is prime
+def isPrime(n):
+	return len(factors(n)) == 2
+
+# finds the next prime number
+def primeList(n):
+
+	lst = []
+	x = 0
+	while len(lst) < n:
+		#iterate to the next number
+		x = x + 1
+		
+		if isPrime(x) == True:
+			lst.append(x)
+		print(lst)
+		
+def primeFactors(n):
+	return [x for x in range(1, n + 1) if (n % x == 0) and (isPrime(x) == True)]
+	
+
+# solves the problem
 def problem3(n):
-	primes = [x for x in range (n) if isPrime(n) == True]
+	return max(primeFactors(n))
+	
